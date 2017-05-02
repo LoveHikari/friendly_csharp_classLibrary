e559f2aa40984afac55d55df3a79d09c9619e002
子丶不语 <xmh.yxw@gmail.com>
2017-05-02 09:54:24 +0800

    添加Dapper

---
+++ 类库积累/DotNet.Utilities/DBHelper/Dapper/DataAnnotations/KeyInfoAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DotNet.Utilities.DBHelper.Dapper.DataAnnotations
+{
+    /// <summary>
+    /// 表示唯一标识实体的一个或多个属性。
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+    public class KeyInfoAttribute : Attribute
+    {
+        
+    }
+}
