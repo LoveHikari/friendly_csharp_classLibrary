ef37f8392d3e08314bd4af99ab0ea0d9bec85e5c
子丶不语 <xmh.yxw@gmail.com>
2017-06-27 16:51:22 +0800

    修改

--- FSLib.Extension/FsLib.Common.Standard/XmlHelper.cs
+++ FSLib.Extension/FsLib.Common.Standard/XmlHelper.cs
@@ -187,6 +187,5 @@ namespace System
 
         #endregion
 
-
     }
 }
---
+++ FSLib.Extension/FsLib.Common/Xml/XmlDocumentHelper.cs
@@ -0,0 +1,54 @@
+﻿namespace System.Xml
+{
+    /// <summary>
+    /// <see cref="XmlDocument"/> 帮助类
+    /// </summary>
+    public class XmlDocumentHelper
+    {
+        private System.Xml.XmlDocument _xml;
+        private string _filePath;
+        /// <summary>
+        /// 从指定的 URL 加载 XML 文档。
+        /// </summary>
+        /// <param name="filePath">含要加载的 XML 文档的文件的 URL。
+        ///    URL 既可以是本地文件，也可以是 HTTP URL（Web 地址）。</param>
+        public XmlDocumentHelper(string filePath)
+        {
+            this._filePath = filePath;
+
+            this._xml = new XmlDocument();
+            this._xml.Load(this._filePath);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="xPath"></param>
+        /// <returns></returns>
+        public string SelectSingleNode(string xPath)
+        {
+            XmlNode xmlNode = _xml.SelectSingleNode(xPath);
+            if (xmlNode != null)
+            {
+                return xmlNode.InnerText;
+            }
+            else
+            {
+                return "";
+            }
+        }
+        /// <summary>
+        /// 保存节点，如果不存在则添加
+        /// </summary>
+        /// <param name="xPath"></param>
+        /// <param name="value"></param>
+        public void SaveXmlNode(string xPath, string value)
+        {
+            XmlNode xmlNode = _xml.SelectSingleNode(xPath)?? _xml.CreateElement(xPath);
+            xmlNode.InnerText = xPath;
+            _xml.Save(_filePath);
+
+
+
+        }
+    }
+}
